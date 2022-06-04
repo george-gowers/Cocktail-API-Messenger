0f9ecd6bf5ff4dca63ef24ae0735da2b879959d1
George Gowers <georgegowers@gmail.com>
2022-06-04 11:12:55 +0200

    added post api method

--- app/controllers/drinks_controller.rb
+++ app/controllers/drinks_controller.rb
@@ -9,7 +9,16 @@ class DrinksController < ApplicationController
     modified_params[:name] = modified_params[:name].capitalize
     @drink = Drink.new(modified_params)
     if @drink.save
-      ##### post to api
+      post_api
+      case @response.code
+      when 201
+        raise
+        redirect_to added_drink_path
+      when 422
+        redirect_to not_unique_path
+      else
+        redirect_to root_path
+      end
     else
       render :new
     end
@@ -19,6 +28,12 @@ class DrinksController < ApplicationController
 
   def drink_params
     params.require(:drink).permit(:name, :ingredients, :preparation, :image)
+  end
+
+  def post_api
+    body = { "cocktail": { "name": @drink.name, "ingredients": @drink.ingredients, "preparation": @drink.preparation, "image": @drink.image } }
+    @response = RestClient.post('https://cocktail-database-api.herokuapp.com/api/v1/cocktails', body){|response, request, result| response }
+    @data = JSON.parse(@response)
   end
 
 end
